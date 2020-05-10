b0b3cb980ab5a2555011bd89935a3109cc93eaef
millord <juancarlosmillord@gmail.com>
2020-05-10 06:43:18 -0400

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+// Styles
+import "../pages/components/styles.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ pages/components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="bg-blue-800 w-full text-center pb-8">
+        <p className="text-xs text-gray-600 font-light">
+          Copyright © 2020 Juan Millord
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
