73236b26851a93489603a1ee8333d2b228754e68
millord <juancarlosmillord@gmail.com>
2020-05-10 07:52:22 -0400

    modified the app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
 // Styles
-import "../pages/components/styles.css";
+// import "../pages/components/styles.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
+
+// export default MyApp;
+
+import React from "react";
+import "../styles/index.css";
+import App from "next/app";
+
+export default class TailwindApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
 }
-
-export default MyApp;
